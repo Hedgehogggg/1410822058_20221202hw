84b9c930938d4833d6222ca06f414fde02e38d15
Hedgehogggg <lklk04569@gmail.com>
2022-12-09 08:54:16 +0800

    finalhw

---
+++ js/web.js
@@ -0,0 +1,45 @@
+        //gsap.to("#BG",
+        //{
+            //x: -1100,
+            //scrollTrigger:
+            //{
+             //start: "10% 15%",
+             //end: "30% 20%",
+             //scrub: true,
+             //markers: true,
+            //}
+        //})
+
+        gsap.to("#WR",
+        {
+            x: 337,
+            scrollTrigger:
+            {
+             start: "10% 15%",
+             end: "30% 20%",
+             scrub: true,
+            }
+        })
+
+        gsap.to("#WL",
+        {
+            x: -337,
+            scrollTrigger:
+            {
+             start: "10% 15%",
+             end: "30% 20%",
+             scrub: true,
+            }
+        })
+
+        gsap.to("#title",
+        {
+            scale: 0.8,
+            y: 150,
+            scrollTrigger:
+            {
+             start: "10% 15%",
+             end: "30% 20%",
+             scrub: true,
+            }
+        })
